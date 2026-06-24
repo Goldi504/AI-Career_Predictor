8a6ad07db2faff745d788bea6c55f02172e14acb
Goldi504 <goldikumari107@gmail.com>
2026-06-24 13:50:26 +0530

    update in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,14 +1,57 @@
-// import errorMiddleware from "./middlewares/error.middleware.js";
+// // import errorMiddleware from "./middlewares/error.middleware.js";
+// // import routes from "./routes/index.js";
+
+// // app.get("/", (req, res) => {
+// //   res.send("API Working");
+// // });
+// // // routes
+// // app.use("/api/v1", routes);
+
+// // // global error handler
+// // app.use(errorMiddleware);
+// import express from "express";
+// import cors from "cors";
+// import cookieParser from "cookie-parser";
+
 // import routes from "./routes/index.js";
+// import errorMiddleware from "./middlewares/error.middleware.js";
+
+// const app = express();
+
+// // app.use(cors({
+// //   origin: "http://localhost:5173",
+// //     credentials: true,
+// // }) );
+
+
+// const cors = require("cors");
+
+// app.use(
+//   cors({
+//     origin: [
+//       "http://localhost:5173",
+//       "https://ai-career-predictor-sigma.vercel.app"
+//     ],
+//     credentials: true,
+//   })
+// );
+// app.use(express.json());
+// app.use(cookieParser());
 
 // app.get("/", (req, res) => {
 //   res.send("API Working");
 // });
-// // routes
+
+
 // app.use("/api/v1", routes);
 
-// // global error handler
 // app.use(errorMiddleware);
+
+// export default app;
+
+
+
+
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
@@ -18,10 +61,16 @@ import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
 
-app.use(cors({
-  origin: "http://localhost:5173",
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://ai-career-predictor-sigma.vercel.app",
+    ],
     credentials: true,
-}) );
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
